1fddbe922eb2442d3e3a0d7b339f45488a549c30
meetalif <meetalif@gmail.com>
2024-03-04 00:20:57 +0600

    ⏫ Update new theme

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "818838f33d73b6cb2b6826d2515b2cb1",
-"/": "818838f33d73b6cb2b6826d2515b2cb1",
-"main.dart.js": "b02f0d5c8791ec11a4fd6aaa7073243d",
+"index.html": "8986d9c87819b89f4131d1a975c421c9",
+"/": "8986d9c87819b89f4131d1a975c421c9",
+"main.dart.js": "65dd94056d5dab7a9a7e99423bb114c5",
 "manifest.json": "e34c8bc8bccf420cf59b3f995a4f320d",
 "version.json": "6c80da4711899cd895f7996b81e823d3"};
 // The application shell files that are downloaded before a service worker can
